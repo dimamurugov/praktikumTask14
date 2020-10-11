0363d151649aec05520389bdfb3db28d86566fd5
Dima Murugov <dimamurugov@gmail.com>
2020-10-11 18:41:44 +0300

    added cheсk node_env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,7 +16,8 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    const { NODE_ENV } = process.env;
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return res
       .status(401)
