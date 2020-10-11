fd7892ca1506576e2473a69a866c1fcb5a377197
Dima Murugov <dimamurugov@gmail.com>
2020-10-11 18:37:52 +0300

    Changed the returned data during authorization

--- controllers/user.js
+++ controllers/user.js
@@ -23,6 +23,7 @@ module.exports.login = (req, res) => {
         httpOnly: true,
         sameSite: true,
       })
+      res.status(200).send({ message: 'Авторизация прошла успешно!' })
         .end();
     })
     .catch(() => {
